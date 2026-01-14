d054e52f80536182a5428c3d462c4f83d9cbc118
David <davidoallan1234@gmail.com>
2026-01-14 11:51:44 +0000

    first commit

--- monblogprojet/settings.py
+++ monblogprojet/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 #         STATIC FILES
 # ===========================
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
